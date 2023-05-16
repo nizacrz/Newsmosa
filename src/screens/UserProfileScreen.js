1eac55c64d70e161f927f14ed1d6f85bfc66ef94
nizacrz <cruz.yuniza@gmail.com>
2023-05-16 08:22:44 +0800

    Changed User Profile Screen UI

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -1,75 +1,88 @@
-import { MaterialIcons } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
-import React, { useEffect, useState } from 'react';
-import { Button, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Card, Paragraph, Title } from 'react-native-paper';
-import { firebase } from '../components/database/firebase';
+import { MaterialIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Card, Paragraph, Title } from "react-native-paper";
+import { firebase } from "../components/database/firebase";
 
 signOut = () => {
-  firebase.auth().signOut().then(() => {
-    this.props.navigation.navigate('Login')
-  })
-  .catch(error => this.setState({ errorMessage: error.message }))
-}  
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      this.props.navigation.navigate("Login");
+    })
+    .catch((error) => this.setState({ errorMessage: error.message }));
+};
 
 const UserProfileScreen = () => {
   const [name, setName] = useState([]);
 
   const changePassword = () => {
-    firebase.auth().sendPasswordResetEmail(firebase.auth().currentUser.email)
-    .then(() => {
-      alert('Password reset email sent!')
-    })
-    .catch(error => {
-      alert(error)
-    })
-  }
+    firebase
+      .auth()
+      .sendPasswordResetEmail(firebase.auth().currentUser.email)
+      .then(() => {
+        alert("Password reset email sent!");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
 
   useEffect(() => {
-    firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid).get()
-    .then((snapshot) =>{
-      if(snapshot.exists){
-          setName(snapshot.data())
-      }
-      else {
-        console.log('does not exist')
-      }
-  })
-  }, [])
-
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setName(snapshot.data());
+        } else {
+          console.log("does not exist");
+        }
+      });
+  }, []);
 
   return (
     <LinearGradient
-      colors={['white', '#176051']}
+      colors={["#FFC371", "#FF5F6D"]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
-      style={styles.container}>
-       <SafeAreaView style={styles.container}>
-      
-      <Text style={{fontSize:20, fontWeight:'bold'}}>
-        Hello, {name.firstName}
-      </Text>
-      <TouchableOpacity
-          onPress={()=>{
-            changePassword()
-        }}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.greeting}>Hello, {name.firstName}!</Text>
+
+        <TouchableOpacity
+          onPress={() => {
+            changePassword();
+          }}
           style={styles.button}
-      >
-        <Text style={{fontWeight:'bold', fontSize:22}}>Change Password</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-          onPress={()=>{
+        >
+          <Text style={styles.buttonText}>Change Password</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
             firebase.auth().signOut();
-        }}
+          }}
           style={styles.button}
-      >
-        <Text style={{fontWeight:'bold', fontSize:22}}>Sign Out</Text>
-      </TouchableOpacity>
-    
-  </SafeAreaView>
-  </LinearGradient>
-  )
-}
+        >
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -78,45 +91,26 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     paddingTop: 10,
   },
-  userImage: {
-    alignSelf: 'center',
-    width: 130,
-    height: 130,
-    borderRadius: 9999,
-    marginTop: 80,
+  greeting: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#181B2E",
+    textAlign: "center",
   },
-  card: {
+  button: {
+    backgroundColor: "#02474C",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 20,
-    borderRadius: 20,
-    shadowOffset: { width: 5, height: 5 },
-    shadowColor: 'black',
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 5,
-    width: '100%',
+    marginBottom: 10,
   },
-  email: {
-    marginTop: 1,
-    fontSize: 11,
-    color: 'gray',
-  },
-  catchphrase: {
-    marginTop: 1,
-    fontSize: 11,
-    color: 'gray',
-  },
-  gender: {
-    marginTop: 20,
-  },
-  link: {
-    color: '#017e60',
-    marginTop: 10,
-  },
-  editIcon: {
-    position: 'absolute',
-    top: 265,
-    right: 60,
-    zIndex: 1,
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 22,
   },
 });
 
